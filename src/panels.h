7a7fd1479a2500788049c26ba8dbf4ea9f1e4e69
tka-andrew <kui.an.andrew.tan@gmail.com>
2022-06-14 21:29:15 +0800

    added buffered painting

--- src/panels.h
+++ src/panels.h
@@ -4,6 +4,8 @@
 #include <wx/wx.h>
 #include <wx/panel.h>
 #include <wx/grid.h>
+#include <wx/dcmemory.h>
+#include <wx/dcbuffer.h>
 
 class LeftPanel : public wxPanel
 {
@@ -34,14 +36,23 @@ public:
     EnvironmentGeometryPanel(wxPanel *parent);
     wxPanel *m_parent;
     wxButton *m_uploadImage;
+    wxMemoryDC *m_memoryDC;
+    wxAutoBufferedPaintDC *m_bufferDC;
+    wxBitmap environmentDrawing;
 
     // REFERENCE: https://wiki.wxwidgets.org/Drawing_on_a_panel_with_a_DC
     // REFERENCE: https://www.informit.com/articles/article.aspx?p=405047
     void OnPaint(wxPaintEvent & evt);
+    void OnErase(wxEraseEvent& event);
     void paintNow(); // actually doesnt need this
     void OnMotion(wxMouseEvent& event);
+    void OnSize(wxSizeEvent& event);
     void render(wxDC& dc);
     wxDECLARE_EVENT_TABLE();
+private:
+    int clientAreaWidth = 10; // dummy value, will be updated OnResize()
+    int clientAreaHeight = 10; // dummy value, will be updated OnResize()
+    bool initialSizeTaken = false;
 };
 
 const int ID_DEFINE_ENVIRONMENT = 101;
