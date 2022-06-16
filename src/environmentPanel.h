22f9c8b84bad2fa54dfe239dea1a228b5f42e44f
tka-andrew <kui.an.andrew.tan@gmail.com>
2022-06-16 19:11:12 +0800

    removed unused variables

--- src/environmentPanel.h
+++ src/environmentPanel.h
@@ -13,20 +13,16 @@ public:
     EnvironmentGeometryPanel(wxPanel *parent);
     wxPanel *m_parent;
     wxButton *m_uploadImage;
-    wxMemoryDC *m_memoryDC;
-    wxAutoBufferedPaintDC *m_bufferDC;
     wxBitmap environmentDrawing;
 
     // REFERENCE: https://wiki.wxwidgets.org/Drawing_on_a_panel_with_a_DC
     // REFERENCE: https://www.informit.com/articles/article.aspx?p=405047
     void OnPaint(wxPaintEvent & evt);
     void OnErase(wxEraseEvent& event);
-    void paintNow(); // actually doesnt need this
     void OnMotion(wxMouseEvent& event);
     void OnLeftMouseClicked(wxMouseEvent& event);
     void OnRightMouseClicked(wxMouseEvent& event);
     void OnSize(wxSizeEvent& event);
-    void render(wxDC& dc);
     wxDECLARE_EVENT_TABLE();
 private:
     int clientAreaWidth = 10; // dummy value, will be updated OnResize()
